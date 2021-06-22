b430ef10613ebde4562ac56cc0a4e46a291f55bb
Kevin <>
2021-06-21 21:03:48 -0700

    Fix hashtag spacing issue and add instruction for users

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -55,7 +55,7 @@ const Form = ({ currentId, setCurrentId }) => {
                 <Typography variant="h6">{ currentId ? "Editing" : "Creating" } a Memory</Typography>
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
                 <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
+                <TextField name="tags" variant="outlined" label="Place Tags Here, Separated By Space" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(', #') })} />
                 <div className={classes.fileInput}>
                     <FileBase type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64 })}/>
                 </div>
